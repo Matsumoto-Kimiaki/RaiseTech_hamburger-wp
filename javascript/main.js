e0fb4bfc087acb6f6956e255141a035ca9f39be7
松本公彰 <gph20078@s.okadai.jp>
2021-11-25 01:14:37 +0900

    スクロールバー修正

--- javascript/main.js
+++ javascript/main.js
@@ -4,11 +4,13 @@ $(function () {
   $(".js-menu").click(function () {
     $(".l-sidebar").addClass("is-open");
     $(".p-menu-bg").addClass("is-open");
+    $("body").addClass("is-open");
   });
 
   $(".c-cross-button").click(function () {
     $(".l-sidebar").removeClass("is-open");
     $(".p-menu-bg").removeClass("is-open");
+    $("body").removeClass("is-open");
   });
 
 });
